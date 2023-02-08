e5b6d25404df726d6de3c9e31d4c5ea8897b7fac
songbinghui <soongbinghui@gmail.com>
2023-02-08 17:55:48 +0800

    feat: 配置完成

--- orval.config.ts
+++ orval.config.ts
@@ -6,16 +6,27 @@ const INPUT = "./src/openapi/spec.yaml";
 export default defineConfig({
   pet: {
     output: {
-      workspace: "src/",
-      target: "./openapi/__service__/swagger.ts",
+      workspace: "src/openapi",
+      target: "./__service__/swagger.ts",
       client: "axios",
       mode: "split",
       mock: true,
       clean: true,
       override: {
         title: (title) => {
-          return 'Api'
+          return "Api";
         },
+        mutator: {
+          path: './custom-instance.ts',
+          name: 'customInstance',
+        },
+        header: (info) => [
+          `此类型文件由swagger解析自动生成，请勿修改`,
+          "<==================================>",
+          info.title,
+          ...(info.version ? [`OpenAPI spec version: ${info.version}`] : []),
+          ...(info.description?.match(/.{1,120}/g) ?? []),
+        ].filter(Boolean),
         components: {
           schemas: {
             suffix: "DTO",
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./openapi/__service__/swagger";
-export * from "./openapi/__service__/swagger.schemas";
